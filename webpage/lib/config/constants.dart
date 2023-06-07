98ad1fc5b38e1fc8645d5e5a8c4ceaf5efab80d9
Femi osibemekun <oo158@student.le.ac.uk>
2023-06-07 21:24:41 +0100

    changes

---
+++ webpage/lib/config/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/sfemi/';
+  static const PROFILE_GITHUB = 'https://github.com/Femlol1/';
+  static const PROFILE_INSTAGRAM = 'https://www.instagram.com/femlol/';
+}
---
+++ webpage/lib/config/themes.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+darkTheme(context) {
+  return ThemeData(
+    fontFamily: 'NotoSans-Regular',
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.black,
+    colorScheme: const ColorScheme.dark()
+        .copyWith(secondary: Colors.blue, primary: Colors.blue),
+    disabledColor: Colors.grey,
+    cardColor: const Color(0xff1f2124),
+    canvasColor: Colors.black,
+    brightness: Brightness.dark,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+      ),
+    ),
+    appBarTheme:
+        const AppBarTheme(elevation: 0.0, backgroundColor: Colors.black),
+  );
+}
+
+lightTheme(context) {
+  return ThemeData(
+    fontFamily: 'NotoSans-Regular',
+    primarySwatch: Colors.blue,
+    primaryColor: Colors.white,
+    colorScheme: const ColorScheme.light()
+        .copyWith(secondary: Colors.blue, primary: Colors.blue),
+    disabledColor: Colors.grey,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+      ),
+    ),
+    cardColor: Colors.white,
+    canvasColor: Colors.white,
+    brightness: Brightness.light,
+    appBarTheme:
+        const AppBarTheme(elevation: 0.0, backgroundColor: Colors.white),
+  );
+}
