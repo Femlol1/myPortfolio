4fbd0db19e3fc03cf621b3ab37b9a7ec02892318
Femi osibemekun <oo158@student.le.ac.uk>
2023-06-12 21:57:11 +0100

    sample1

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,152 @@
+// import 'package:flutter/material.dart';
+// import 'package:webpage/responsive/desktop_body.dart';
+// import 'package:webpage/responsive/mobile_body.dart';
+// import 'package:webpage/responsive/responsive_layout.dart';
+// import 'package:webpage/responsive/tablet_body.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Osifemi Osibemekun',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(primarySwatch: Colors.green),
+//       home: ResponsiveLayout(
+//         mobileBody: const MobileScaffold(),
+//         tabletBody: const TabletScaffold(),
+//         desktopBody: const DesktopScaffold(),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:webpage/responsive/desktop_body.dart';
-import 'package:webpage/responsive/mobile_body.dart';
-import 'package:webpage/responsive/responsive_layout.dart';
-import 'package:webpage/responsive/tablet_body.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(PortfolioApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class PortfolioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Osifemi Osibemekun',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: ResponsiveLayout(
-        mobileBody: const MobileScaffold(),
-        tabletBody: const TabletScaffold(),
-        desktopBody: const DesktopScaffold(),
+      title: 'My Portfolio',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PortfolioHomePage(),
+    );
+  }
+}
+
+class PortfolioHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Portfolio'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage('assets/profile_picture.jpg'),
+              ),
+            ),
+            Text(
+              'Your Name',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'About Me',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'Write a brief description about yourself and your skills. '
+                'You can mention your experience, education, and any notable projects you have worked on.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Skills',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: [
+                Chip(label: Text('Flutter')),
+                Chip(label: Text('Dart')),
+                Chip(label: Text('Firebase')),
+                // Add more skills here
+              ],
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Projects',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 3, // Replace with the actual number of projects
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Project Title'),
+                  subtitle: Text('Project description'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // Navigate to the project details page
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/responsive/responsive_layout.dart
+++ lib/responsive/responsive_layout.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const int largeSreenSize = 1366;
+const int mediumSreenSize = 768;
+const int smallSreenSize = 360;
+const int customSreenSize = 1100;
 class ResponsiveLayout extends StatelessWidget {
   final Widget mobileBody;
   final Widget tabletBody;
